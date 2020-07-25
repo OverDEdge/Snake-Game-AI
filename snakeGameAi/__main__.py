e4b340c9d01fbaf68299b4181b510c8a38f4f111
Niklas Moberg <niklasm85@gmail.com>
2020-07-25 22:36:04 +0200

    Updated __main__.py to run the NEAT function in main at program start with the added config file

--- snakeGameAi/__main__.py
+++ snakeGameAi/__main__.py
@@ -1,4 +1,6 @@
 from snakeGameAi import main
 
 if __name__ == '__main__':
-    pass
+    local_dir = path.dirname(__file__)
+    config_path = path.join(local_dir, "config-feedforward.txt")
+    main.run(config_path)
