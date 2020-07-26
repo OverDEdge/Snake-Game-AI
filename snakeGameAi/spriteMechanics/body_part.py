f3dd554bd49a283fb36f0e37071f02dd9488c92c
Niklas Moberg <niklasm85@gmail.com>
2020-07-26 08:58:13 +0200

    Updated body_parts.py to handle row, col as position and to use snake index to add to correct group for multiple snakes

--- snakeGameAi/spriteMechanics/body_part.py
+++ snakeGameAi/spriteMechanics/body_part.py
@@ -4,17 +4,19 @@ from .settings import vec
 from .sprite_methods import remove_background_from_img
 
 class BodyPart(pg.sprite.Sprite):
-    def __init__(self, game, snake, x, y):
+    def __init__(self, game, snake, col, row, index):
         self._layer = settings.BODY_LAYER
-        self.groups = game.all_sprites, game.snake_body
+        self.groups = game.all_sprites, game.snake_body_groups[index]
         pg.sprite.Sprite.__init__(self, self.groups)
+        self.index = index
         self.game = game
         self.image = pg.transform.rotate(self.game.snake_spritesheet.get_image(*settings.SNAKE_IMG_TAIL, settings.SIZE), 90)
-        self.image = remove_background_from_img(self.image, settings.BLACK)
+        self.image = remove_background_from_img(self.image, settings.WHITE)
+
         self.rect = self.image.get_rect()
-        self.pos = vec(x, y) * settings.TILESIZE
-        self.rect.topleft = self.pos
+        self.pos = vec(int(col), int(row))
+        self.rect.topleft = self.pos * settings.TILESIZE
 
     def update(self):
         # Update rectangle
-        self.rect.topleft = self.pos
+        self.rect.topleft = self.pos * settings.TILESIZE
