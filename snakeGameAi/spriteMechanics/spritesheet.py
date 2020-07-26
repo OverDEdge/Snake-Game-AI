4e69ee0065ecccc9bef8f85bcd3f62e5bb11c63c
Niklas Moberg <niklasm85@gmail.com>
2020-07-26 09:03:30 +0200

    Just changed to use col and row instead of x and y (for clarity) for spritesheet.py

--- snakeGameAi/spriteMechanics/spritesheet.py
+++ snakeGameAi/spriteMechanics/spritesheet.py
@@ -6,9 +6,9 @@ class Spritesheet:
     def __init__(self, filename):
         self.spritesheet = pg.image.load(filename).convert()
 
-    def get_image(self, x, y, width, height, size):
+    def get_image(self, col, row, width, height, size):
         # Grab an image out of a larger Spreadsheet
         image = pg.Surface((width, height))
-        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
+        image.blit(self.spritesheet, (0, 0), (col, row, width, height))
         image = pg.transform.scale(image, (int(size[0]), int(size[1])))
         return image
