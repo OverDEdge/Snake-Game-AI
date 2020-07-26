8f58504e9141cfa00425e03f0efd9dd1a14e6c49
Niklas Moberg <niklasm85@gmail.com>
2020-07-26 09:00:48 +0200

    Updated food.py to use row, col as position. Also removed the random movement to the food (for now). Finally added to use snake index to add to correct group for multiple snakes.

--- snakeGameAi/spriteMechanics/food.py
+++ snakeGameAi/spriteMechanics/food.py
@@ -6,10 +6,11 @@ from .settings import vec
 from .sprite_methods import check_for_collision, remove_background_from_img
 
 class Food(pg.sprite.Sprite):
-    def __init__(self, game):
+    def __init__(self, game, index):
         self._layer = settings.FOOD_LAYER
         self.game = game
-        self.groups = game.all_sprites, game.food
+        self.groups = game.all_sprites, game.food_groups[index]
+        self.index = index
         pg.sprite.Sprite.__init__(self, self.groups)
         self.image = pg.transform.scale(pg.image.load(path.join(settings.IMG_FOLDER, settings.FOOD_IMG)).convert(), settings.SIZE)
         self.image = remove_background_from_img(self.image, settings.BLACK)
@@ -22,25 +23,12 @@ class Food(pg.sprite.Sprite):
         Places food at a random location, checking that a sprite doesn't already
         exist there.
         '''
-        self.pos = vec(random.randrange(settings.GRIDWIDTH - 1), random.randrange(settings.GRIDHEIGHT - 1)) * settings.TILESIZE
-        self.rect.topleft = self.pos
-        if check_for_collision(self, self.game.walls, False) or check_for_collision(self, self.game.snake_body, False):
+        self.col = random.randrange(settings.GRIDWIDTH - 1)
+        self.row = random.randrange(settings.GRIDHEIGHT - 1)
+        self.pos = vec(self.col, self.row)
+        self.rect.topleft = self.pos * settings.TILESIZE
+        if check_for_collision(self, self.game.walls, False) or check_for_collision(self, self.game.snake_body_groups[self.index], False):
             self.place_food()
 
     def update(self):
-        now = pg.time.get_ticks()
-
-        if (now - self.pos_update_time) > settings.FOOD_POS_UPDATE_RATE:
-            self.get_new_position()
-            self.pos_update_time = now
-
-    def get_new_position(self):
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        dir = random.choice(directions)
-        self.pos.x += dir[0] * settings.TILESIZE
-        self.pos.y += dir[1] * settings.TILESIZE
-        self.rect.topleft = self.pos
-        if check_for_collision(self, self.game.walls, False) or check_for_collision(self, self.game.snake_body, False):
-            self.pos.x -= dir[0] * settings.TILESIZE
-            self.pos.y -= dir[1] * settings.TILESIZE
-            self.get_new_position()
+        pass
